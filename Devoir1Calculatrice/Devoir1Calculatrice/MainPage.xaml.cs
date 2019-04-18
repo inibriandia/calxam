892301ce5ab9539a9b95a89bd876e291d6d11d20
inibriandia <inibriandia@gmail.com>
2019-04-18 10:50:52 -0300

    Ajouter la calculatrice xamarin

---
+++ Devoir1Calculatrice/Devoir1Calculatrice/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿/*
+ Programmeur: Ange-Christian Silue & Briand Leblanc
+ Date       :
+ but        :
+ 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Devoir1Calculatrice
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            // LoadApplication(new HelloWorld.App());
+             
+        }
+    }
+}
